a3d39af2917ad27d94fa080c6ba2305c3673a3b7
DNA <38492909+theblackdna@users.noreply.github.com>
2023-02-26 09:45:59 +0800

    Update DocumentMeta.tsx

--- src/lib/components/DocumentMeta.tsx
+++ src/lib/components/DocumentMeta.tsx
@@ -1,16 +1,16 @@
 import { UMAMI_SRC, UMAMI_WEBSITE_ID } from '~/lib/constants/umami';
 
-const APP_NAME = 'sznm.dev';
+const APP_NAME = 'dnachavez.com';
 
 const DocumentMeta = () => {
   return (
     <>
-      <link rel="icon" href="/avataaars.svg" />
+      <link rel="icon" href="/logo.png" />
       <link
         rel="alternate"
         type="application/rss+xml"
-        title="RSS feed for sozonome's blog posts"
-        href="https://sznm.dev/rss.xml"
+        title="RSS feed for dnachavez's blog posts"
+        href="https://dnachavez.com/rss.xml"
       />
 
       <meta name="application-name" content={APP_NAME} />
@@ -29,7 +29,7 @@ const DocumentMeta = () => {
         defer
         data-website-id={UMAMI_WEBSITE_ID}
         src={UMAMI_SRC}
-        data-domains="sznm.dev"
+        data-domains="dnachavez.com"
       />
     </>
   );
