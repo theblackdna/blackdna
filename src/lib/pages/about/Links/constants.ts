831457332e9859c49cbb20dcfd80bfc77710922e
DNA <38492909+theblackdna@users.noreply.github.com>
2023-02-26 09:29:08 +0800

    Update constants.ts

--- src/lib/pages/about/Links/constants.ts
+++ src/lib/pages/about/Links/constants.ts
@@ -15,47 +15,47 @@ import type { MotionLinkProps } from './types';
 export const socialLinks: Array<MotionLinkProps> = [
   {
     platformName: 'GitHub',
-    url: 'https://github.com/sozonome',
+    url: 'https://github.com/dnachavez',
     icon: FaGithub,
   },
   {
     platformName: 'LinkedIn',
-    url: 'https://linkedin.com/in/agustinusnathaniel',
+    url: 'https://linkedin.com/in/dnachavez',
     icon: FaLinkedin,
   },
   {
     platformName: 'Twitter',
-    url: 'https://twitter.com/sozonome',
+    url: 'https://twitter.com/dnachavez',
     icon: FaTwitter,
   },
   {
     platformName: 'Instagram',
-    url: 'https://instagram.com/sozonome',
+    url: 'https://instagram.com/dnachavez',
     icon: FaInstagram,
   },
   {
     platformName: 'YouTube',
-    url: 'https://youtube.com/channel/UCJnYMGIHtQ8yInuq4Pc2Ttg',
+    url: 'https://youtube.com/channel/superdanglyde',
     icon: FaYoutube,
   },
   {
     platformName: 'dev.to',
-    url: 'https://dev.to/sozonome',
+    url: 'https://dev.to/dnachavez',
     icon: FaDev,
   },
   {
     platformName: 'Medium',
-    url: 'https://medium.com/@agustinusnathaniel',
+    url: 'https://medium.com/@dnachavez',
     icon: FaMedium,
   },
   {
     platformName: 'ProductHunt',
-    url: 'https://www.producthunt.com/@sozonome/made',
+    url: 'https://www.producthunt.com/@dnachavez/made',
     icon: FaProductHunt,
   },
   {
     platformName: 'e-mail',
-    url: 'mailto:hello@sznm.dev?cc=agustinusnathaniel228@gmail.com',
+    url: 'mailto:dna.chavez@outlook.com',
     icon: FaEnvelope,
   },
 ];
